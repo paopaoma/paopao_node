e71dfa2cd7ea7b4690fa5f4f75bd2ecf97559f64
masikai <masikai@bytedance.com>
2023-05-19 11:27:15 +0800

    feat：add html

--- src/controller/moment.controller.js
+++ src/controller/moment.controller.js
@@ -27,7 +27,6 @@ class MomentController {
   async list(ctx, next) {
     // 1.获取数据(offset/size)
     const { offset, size } = ctx.query;
-
     // 2.查询列表
     const result = await momentService.getMomentList(offset, size);
     ctx.body = result;
--- src/main.js
+++ src/main.js
@@ -6,9 +6,12 @@ require('./app/database');
 const config = require('./app/config');
 
 app.use(ctx => {
-  ctx.type = "html";
-  console.log("2222");
-  ctx.body = fs.readFileSync(path.resolve(__dirname, "./src/pages/index.html"));
+  try {
+    ctx.set("Content-Type", "text/html");
+    ctx.body = fs.readFileSync(path.resolve(__dirname, "./pages/index.html"));
+  } catch (err) {
+    console.log(err);
+  }
 }).listen(config.APP_PORT, () => {
   console.log(`服务器在${config.APP_PORT}端口启动成功~`);
 });
